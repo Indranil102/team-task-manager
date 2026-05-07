b8f1573a9e658a5eb727ac4a7bb15c36f841b872
Indranil102e <indranilsamanta2003@gmail.com>
2026-05-08 01:43:53 +0530

    backend work fine and singup data go to databse

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,45 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import API from "../services/api";
 
 function Signup() {
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSignup = async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+      await API.post(
+        "/auth/signup",
+        formData
+      );
+
+      navigate("/");
+
+    } catch (error) {
+
+      console.log(error.response.data);
+
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-50">
 
@@ -10,23 +49,35 @@ function Signup() {
           Create Account
         </h1>
 
-        <form className="space-y-4">
+        <form
+          onSubmit={handleSignup}
+          className="space-y-4"
+        >
 
           <input
             type="text"
+            name="name"
             placeholder="Full Name"
+            value={formData.name}
+            onChange={handleChange}
             className="w-full border border-slate-300 rounded-lg px-4 py-3 outline-none"
           />
 
           <input
             type="email"
+            name="email"
             placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
             className="w-full border border-slate-300 rounded-lg px-4 py-3 outline-none"
           />
 
           <input
             type="password"
+            name="password"
             placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
             className="w-full border border-slate-300 rounded-lg px-4 py-3 outline-none"
           />
 
